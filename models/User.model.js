2a956a1fac8e8b3ca88fb01e8c9fbd22ef38f7be
yeatsg <yeatsg@gmail.com>
2022-02-10 16:24:36 -0500

    baby steps uwu

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
+
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
+      unique: true,
+      required: true,
     },
-    password: String,
+    password: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    fullName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
   },
   {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
 );
